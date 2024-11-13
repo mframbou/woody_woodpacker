b4b24b45a0608cfe8b7eb13fd6e6bcff836ce143
Med <mederis.frambourt@gmail.com>
2024-11-13 21:17:06 +0100

    We successfully get .text section in ASM, now need to find out why decrypt doesnt work

--- srcs/asm/woody.s
+++ srcs/asm/woody.s
@@ -1,7 +1,7 @@
 .intel_syntax noprefix # use intel syntax (dest, src) and no prefix (%rax -> rax)
 
 base:
-    jmp payload             # 2 bytes: eb 16
+    jmp decrypt_data             # 2 bytes: eb 16
 
 # VARIABLES
 entry_delta:                     # next 8 bytes hold this variable 
@@ -20,14 +20,11 @@ decrypt_offset:                  # offset of section to decrypt
 # DECRYPTION
 decrypt_data:                    # Decrypt data at decrypt_offset of size decrypt_size with decryption_key TEA algorithm
                                  # simple XOR decryption
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
     mov rax, [rip + decrypt_size]
-    mov rbx, [rip + decrypt_offset]
+    mov r8, [rip + decrypt_offset]
+    lea r9, [rip + base]           # address of the base of the code (find payload REAL base address)
+    add r8, r9                     # real address of .text section (payload REAL base address + decrypt_offset)
+    mov rbx, r8                    # real address of the data to decrypt (.text section address)
     mov rcx, [rip + decryption_key]
     mov rdx, 0                   # counter (i)
 
@@ -67,12 +64,6 @@ payload:
     mov rax, 1                   # sys_write
     syscall
 
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
-    mov rax, [rip + entry_delta]
     mov rax, [rip + entry_delta] # delta between old and new entry (need to use rip-relative addressing otherwise the code will not be position independent => segfault)
     lea r10, [rip + base]        # address of the payload (this code)
     sub r10, rax                 # address of the original entry, working with position independent code
