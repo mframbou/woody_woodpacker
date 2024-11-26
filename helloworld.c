680bec1b0fd6645cd417ee739d4c1e0165f293d2
mframbou <mederis.frambourt@gmail.com>
2024-11-26 15:57:46 +0100

    clean repo

--- helloworld.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <stdlib.h>
-
-int main() {
-	write(1, "Hello world\n", 12);
-	exit(0);
-	write(1, "Ta gueule nono\n", 14);
-}
--- pouet.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-#include <stdlib.h>
-
-int main() {
-	write(1, "Testos\n", 7);
-}
